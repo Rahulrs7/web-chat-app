30037ee1eacd3c1cfde86dc4d2e78028d7a565ed
Rahulrs7 <Rrahul5223u@gmail.com>
2024-05-13 12:16:31 +0530

    first commit

---
+++ src/misc/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/compat/app"
+const config={
+apiKey: "AIzaSyDeBBxyl7DFXc_lBVV1hD4JNBv1VrinNFM",
+authDomain: "chat-web-app-a680f.firebaseapp.com",
+projectId: "chat-web-app-a680f",
+storageBucket: "chat-web-app-a680f.appspot.com",
+messagingSenderId: "265601131615",
+appId: "1:265601131615:web:38a8304f8452f90ea7a7ea"
+};
+const app=firebase.initializeApp(config);
